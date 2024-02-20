a58215d7ba697aef6289c877086224c10f9c3e6a
Himanshu Singh Negi <himanshunegi378@gmail.com>
2024-02-20 20:41:52 +0530

    notification added

--- NotificationPlugin/code.js
+++ NotificationPlugin/code.js
@@ -1,3 +1,51 @@
+function showNotification(title) {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    return createNotification("TimerCards", {
+      body: title,
+      vibrate: [200, 100, 200, 100, 200, 100, 200],
+    });
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        return createNotification("TimerCards", {
+          body: title,
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+        });
+      }
+    });
+  }
+}
+
+function createNotification(title, options) {
+  try {
+    new Notification(title, options);
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+  navigator.serviceWorker
+    .getRegistration()
+    .then(function (registration) {
+      if (!registration) return false;
+      registration.showNotification("TimerCards", options);
+      return true;
+    })
+    .catch((err) => {
+      console.log(err);
+      return false;
+    });
+}
+
 ctx.timerCard.on("timerFinished", async (timerId) => {
   const cardName = ctx.timerCard.timerCardData.timerGroup.name;
   const timerData = ctx.timerCard.timerCardData.timerGroup.timers.find(
